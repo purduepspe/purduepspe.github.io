f97bc99f4164968a92e043bff0c89f028322f5cc
delta97 <parson13@purdue.edu>
2019-04-19 14:53:53 -0400

    exec

---
+++ exec area - wip/submit_office_hours.php
@@ -0,0 +1,11 @@
+<?php
+	$name = $_REQUEST['full-name'];
+	$date = $_REQUEST['date'];
+	$start_time = $_REQUEST['start-time'];
+	$end_time = $_REQUEST['end-time'];
+
+
+	//send request to submit these data to the sql server
+
+	//re render the next two weeks 
+?>
