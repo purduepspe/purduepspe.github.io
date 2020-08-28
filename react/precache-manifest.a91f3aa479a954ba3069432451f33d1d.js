ee3a9a8227bc9bc1758f5e09eedf38da02473711
Cole Parsons <cole.parsons@procusus.com>
2020-08-28 03:16:19 -0400

    adding react to /react

---
+++ react/precache-manifest.a91f3aa479a954ba3069432451f33d1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29911ad218638bc0a07fd872fc2a3108",
+    "url": "/index.html"
+  },
+  {
+    "revision": "476497fba67559e01fc7",
+    "url": "/static/css/2.dabebd4b.chunk.css"
+  },
+  {
+    "revision": "94dca38296559aad0a85",
+    "url": "/static/css/main.59d67395.chunk.css"
+  },
+  {
+    "revision": "476497fba67559e01fc7",
+    "url": "/static/js/2.66b0619f.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.66b0619f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94dca38296559aad0a85",
+    "url": "/static/js/main.307ae163.chunk.js"
+  },
+  {
+    "revision": "3e7f8fddc381c7e1c8bc",
+    "url": "/static/js/runtime-main.8b2bd217.js"
+  },
+  {
+    "revision": "ab37371bea9532ca3da938ca9553fdb0",
+    "url": "/static/media/fav.ab37371b.png"
+  }
+]);
